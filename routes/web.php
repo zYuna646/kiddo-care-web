2c43706e4c85bd14804f657cfbcb06c5ec2cb337
zYuna646 <anggarangga646@gmail.com>
2024-01-11 02:35:49 +0800

    base application

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\FrontPageController;
+use App\Http\Controllers\InformationController;
+use App\Http\Controllers\MainSliderController;
+use App\Http\Controllers\ReviewSliderController;
+use App\Http\Controllers\VideoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
